520c7fccb6d642a3ccf0dea08178e08ba8fa943b
TheRealRondon <amirhossein2324236@gmail.com>
2022-07-25 17:51:47 +0430

    Added category serializer.

--- torob/api/models.py
+++ torob/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=100)
     level = models.PositiveSmallIntegerField()
-    parent_id = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"Category => Id: {self.id}, Title: {self.title}, Level: {self.level}, Parent: {self.parent_id}"
