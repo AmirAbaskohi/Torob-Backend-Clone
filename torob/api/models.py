51dacf15de7e7dc0cfdb76bb3210442c91714bd4
TheRealRondon <amirhossein2324236@gmail.com>
2022-07-25 17:30:38 +0430

    Project intialized.

---
+++ torob/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    level = models.PositiveSmallIntegerField()
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Category => Id: {self.id}, Title: {self.title}, Level: {self.level}, Parent: {self.parent_id}"
