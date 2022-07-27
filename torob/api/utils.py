6b4e114f48265c33705e25d66daab97a1ab4b9f1
TheRealRondon <amirhossein2324236@gmail.com>
2022-07-27 15:05:17 +0430

    Updated generate uuid to prevent same uuids.

--- torob/api/utils.py
+++ torob/api/utils.py
@@ -1,5 +1,10 @@
 import string
 import random
+from .models import Product
 
 def generate_uuid(length=12):
-    return ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(length))
+    uuids = set(Product.objects.values_list('uuid', flat=True))
+    generated_uuid = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(length))
+    while generated_uuid in uuids:
+        generated_uuid = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(length))
+    return generated_uuid
